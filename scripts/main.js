34a04acd48b2e13c6926f7b52bb6b58626d4bbe6
Riya Sen <sen.r@northeastern.edu>
2024-01-24 15:15:47 -0500

    End of homework 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
 function openPhotographyPage() {
-  window.open("https://www.instagram.com/thatsriyadiculous/?hl=en", "_blank")
+  window.open("https://www.instagram.com/thatsriyadiculous/?hl=en", "_blank");
 }
+
+window.alert ("Fun Fact: The Earl of Sandwich, John Montagu, who lived in the 1700s, reportedly invented the sandwich so he wouldnt have to leave his gambling table to eat. (pbs.org)");
