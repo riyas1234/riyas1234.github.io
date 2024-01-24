fa88c8a0a8a673ebe74bdad611245413d3bc29e3
Riya Sen <sen.r@northeastern.edu>
2024-01-24 11:27:50 -0500

    Open instagram button

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,3 @@
-// Personalized welcome message code
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-  }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
-
 function openPhotographyPage() {
   window.open("https://www.instagram.com/thatsriyadiculous/?hl=en", "_blank")
 }
